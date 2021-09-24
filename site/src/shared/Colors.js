23d432a3206cdc7c92836b1cce675ef15d2c1259
gisele-araujo <gisele.flor@bandtec.com.br>
2021-09-23 22:22:22 -0300

    add estrutura de pastas e arquivos

---
+++ site/src/shared/Colors.js
@@ -0,0 +1,35 @@
+export const Colors = {
+    gray: {
+        white: '#FFF',
+        hot: '#8B80A5',
+        light: '#5E5769',
+        medium: '#34313A',
+        dark: '#2C2831',
+        darkPurple: '#211F26'
+    },
+    pink: {
+        hot: '#F41B61',
+        dark: '#BD1574',
+        secondHot: '#F72585',
+        secondDark: '#B5179E'
+    },
+    purple: {
+        light: '#75118A',
+        medium: '#591381',
+        dark: '#3F1378',
+        secondLight: '#7209B7',
+        secondMedium: '#560BAD',
+        secondDark: '#480CA8',
+        secondDarker: '#3A0CA3',
+    },
+    blue:{
+        light: '#52C1CC',
+        mediumLight: '#4F8AC8',
+        medium: '#4B59C5',
+        dark: '#46359C',
+        darkPurple: '#3F1378',
+        secondLight: '#4361EE',
+        secondMediumLighter: '#4895EF',
+        secondMedium: '#4CC9F0'
+    }
+}
